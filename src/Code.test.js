69afbe7232a4ef13cd159bfd31aba6d86151ce3b
Grimz <andogreymark@gmail.com>
2020-03-05 14:02:59 +0800

    Fix the error

--- src/Code.test.js
+++ src/Code.test.js
@@ -4,5 +4,5 @@ test('check getFormdata', () => {
   let name= 'Mark Andog';
   let email = 'andogreymark@gmail.com';
   let birthday = '10/08/1995';
-  expect(getFormDataUI([name, email, birthday])).toBe('Mark Andog', 'andogreymark@gmail.com', '10/08/1995')
+  expect(getFormDataUI(name, email, birthday)).toBe('Mark Andog', 'andogreymark@gmail.com', '10/08/1995')
 });
